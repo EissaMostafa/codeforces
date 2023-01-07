0e95a0051a631d6ae79d8d0eef608fb53438448c
Mostafa Eissa <moaeissa@gmail.com>
2023-01-07 21:59:58 +0100

    add 1768a solution

---
+++ problemset/1768a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+#define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
+
+#define ll long long
+
+// #define TEST_MOD
+
+using namespace std;
+
+int solver(ll inp) {
+    int ret_val = 0;
+
+    return ret_val;
+}
+
+int main() {
+    fast int t;
+#ifdef TEST_MOD
+    t = 1;
+#else
+    cin >> t;
+#endif
+    while (t--) {
+        ll inp;
+#ifdef TEST_MOD
+        inp = 4;
+#else
+        cin >> inp;
+#endif
+        cout << inp - 1 << "\n";
+    }
+}
