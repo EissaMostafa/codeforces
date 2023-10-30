708d89014dc95c04024aa4d90f213d78f060fb46
Mostafa Eissa <moaeissa@gmail.com>
2023-10-30 01:09:59 +0100

    fix formatting

--- problemset/1699a.cpp
+++ problemset/1699a.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <vector>
- 
+
 #define fast ios_base::sync_with_stdio(false), cin.tie(NULL);
- 
+
 using namespace std;
- 
-int main(){
-    fast
-    int t;
+
+int main() {
+    fast int t;
     cin >> t;
-    while(t--){
+    while (t--) {
         int inp;
         cin >> inp;
-        if(inp%2 == 0){
-            cout << inp/2 << " 0" << " 0" << "\n";
-        } else{
-            cout << "-1" << "\n";
+        if (inp % 2 == 0) {
+            cout << inp / 2 << " 0"
+                 << " 0"
+                 << "\n";
+        } else {
+            cout << "-1"
+                 << "\n";
         }
     }
 }
